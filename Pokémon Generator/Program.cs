17e1d9337c6ba2a2445d97732038147934139fd4
EliasGoransson07EEDAT23 <elgo07001@utb.vaxjo.se>
2025-01-24 14:44:21 +0100

    20 pokemon added

--- Pokémon Generator/Program.cs
+++ Pokémon Generator/Program.cs
@@ -16,48 +16,118 @@ namespace pokemon
                 while(fisk < 6)
                 {
                     Random random = new Random();
-                    int pokemon = random.Next(4);
+                    int pokemon = random.Next(14);
                     switch(pokemon)
                     {
                         case 0:
                             {
-                                Console.WriteLine("Choose");
+                                Console.WriteLine("#???? MissingNo. (Choose a Pokémon Freely!)");
                                 break;
                             }
                         case 1:
                             {
-                                Console.WriteLine("#1 Bulbasaur");
+                                Console.WriteLine("#1 Bulbasaur ");
                                 break;
                             }
                         case 2:
                             {
-                                Console.Write("#38 Ninetales ");
-                                Random t = new Random();
-                                int type = t.Next(2);
-                                if (type == 0)
-                                {
-                                    Console.WriteLine("Normal");
-                                }
-                                else if (type == 1)
-                                {
-                                    Console.WriteLine("Alolan");
-                                }
+                                Console.WriteLine("#2 Ivysaur ");
                                 break;
                                
                             }
                         case 3:
                             {
-                                Console.WriteLine("#4 Charmander");
+                                Console.WriteLine("#3 Venusaur ");
                                 break;
                             }
-                        default:
+                        case 4:
                             {
-                                Console.WriteLine("Non existent (error 404)");
+                                Console.WriteLine("#4 Charmander ");
+                                break;
+                            }
+                        case 5:
+                            {
+                                Console.WriteLine("#5 Charmeleon ");
+                                break;
+                            }
+                        case 6:
+                            {
+                                Console.WriteLine("#6 Charizard ");
+                                break;
+                            }
+                        case 7:
+                            {
+                                Console.WriteLine("#7 Squirtle ");
+                                break;
+                            }
+                        case 8:
+                            {
+                                Console.WriteLine("#8 Wartortle ");
+                                break;
+                            }
+                        case 9:
+                            {
+                                Console.WriteLine("#9 Blastoise ");
+                                break;
+                            }
+                        case 10:
+                            {
+                                Console.WriteLine("#10 Caterpie ");
+                                break;
+                            }
+                        case 11:
+                            {
+                                Console.WriteLine("#11 Metapod ");
+                                break;
+                            }
+                        case 12:
+                            {
+                                Console.WriteLine("#12 Butterfree ");
+                                break;
+                            }
+                        case 13:
+                            {
+                                Console.WriteLine("#13 Weedle ");
+                                break;
+                            }
+                        case 14:
+                            {
+                                Console.WriteLine("#14 Kakuna ");
+                                break;
+                            }
+                        case 15:
+                            {
+                                Console.WriteLine("#15 Beedrill ");
+                                break;
+                            }
+                        case 16:
+                            {
+                                Console.WriteLine("#16 Pidgey ");
+                                break;
+                            }
+                        case 17:
+                            {
+                                Console.WriteLine("#17 Pidgeotto ");
+                                break;
+                            }
+                        case 18:
+                            {
+                                Console.WriteLine("#18 Pidgeot ");
+                                break;
+                            }
+                        case 19:
+                            {
+                                Console.WriteLine("#19 Rattata ");
+                                break;
+                            }
+                        case 20:
+                            {
+                                Console.WriteLine("#20 Raticate ");
                                 break;
                             }
                     }
                     fisk++;
-                    Thread.Sleep(1800);
+                    Thread.Sleep(10);
                     
                 }
                 Console.WriteLine("Team End");
